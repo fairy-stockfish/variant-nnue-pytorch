70fb22609fa842ce71aa856563e5ca6b4b2d479e
Fabian Fichter <ianfab@users.noreply.github.com>
2021-08-28 12:06:26 +0200

    Changes for shogi

--- lib/nnue_training_data_formats.h
+++ lib/nnue_training_data_formats.h
@@ -54,12 +54,12 @@ THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include <intrin.h>
 #endif
 
-#define FILES 8
-#define RANKS 8
-#define PIECE_TYPES 6
-#define PIECE_COUNT 32
-#define POCKETS false
-#define KING_SQUARES FILES * RANKS
+#define FILES 9
+#define RANKS 9
+#define PIECE_TYPES 10
+#define PIECE_COUNT 40
+#define POCKETS true
+#define KING_SQUARES 1
 
 namespace chess
 {
--- variant.py
+++ variant.py
@@ -1,16 +1,20 @@
-RANKS = 8
-FILES = 8
+RANKS = 9
+FILES = 9
 SQUARES = RANKS * FILES
-KING_SQUARES = RANKS * FILES
-PIECE_TYPES = 6
+KING_SQUARES = 1
+PIECE_TYPES = 10
 PIECES = 2 * PIECE_TYPES
-USE_POCKETS = False
+USE_POCKETS = True
 POCKETS = 2 * FILES if USE_POCKETS else 0
 
 PIECE_VALUES = {
-    1 : 126,
-    2 : 781,
-    3 : 825,
-    4 : 1276,
-    5 : 2538,
+    1: 425,
+    2: 676,
+    3: 360,
+    4: 900,
+    5: 50,
+    6: 200,
+    7: 220,
+    8: 360,
+    9: 750,
 }
