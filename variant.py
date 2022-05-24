97f86be93b4379ef3dd7cd4cf0c17d67bf00d68c
Fabian Fichter <ianfab@users.noreply.github.com>
2022-05-24 09:15:12 +0200

    Changes for xiangqi

--- variant.py
+++ variant.py
@@ -1,16 +1,17 @@
-RANKS = 8
-FILES = 8
+RANKS = 10
+FILES = 9
 SQUARES = RANKS * FILES
-KING_SQUARES = RANKS * FILES
-PIECE_TYPES = 6
+KING_SQUARES = 9
+PIECE_TYPES = 7
 PIECES = 2 * PIECE_TYPES
 USE_POCKETS = False
 POCKETS = 2 * FILES if USE_POCKETS else 0
 
 PIECE_VALUES = {
-    1 : 126,
-    2 : 781,
-    3 : 825,
-    4 : 1276,
-    5 : 2538,
+    1: 1350,
+    2: 320,
+    3: 650,
+    4: 150,
+    5: 600,
+    6: 300,
 }
