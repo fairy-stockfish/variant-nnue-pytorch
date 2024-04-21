ac36757ecd4f64ea3c7492851e16dcf6997aebf8
dpldgr <144766863+dpldgr@users.noreply.github.com>
2024-04-21 22:02:35 +0800

    Moved variant specific #define statements into a separate header file (#32)

--- lib/nnue_training_data_formats.h
+++ lib/nnue_training_data_formats.h
@@ -54,13 +54,7 @@ THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include <intrin.h>
 #endif
 
-#define FILES 8
-#define RANKS 8
-#define PIECE_TYPES 6
-#define PIECE_COUNT 32
-#define POCKETS false
-#define KING_SQUARES FILES * RANKS
-#define DATA_SIZE 512
+#include "../variant.h"
 
 static_assert(DATA_SIZE % 8 == 0);
 
---
+++ variant.h
@@ -0,0 +1,7 @@
+#define FILES 8
+#define RANKS 8
+#define PIECE_TYPES 6
+#define PIECE_COUNT 32
+#define POCKETS false
+#define KING_SQUARES FILES * RANKS
+#define DATA_SIZE 512
